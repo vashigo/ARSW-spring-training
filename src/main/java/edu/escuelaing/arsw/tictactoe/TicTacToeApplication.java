3e13c3638fa35f33116565e880fe9e1175b29d85
Andres Vasquez <andres.vasquez@hydra.cloud>
2020-06-18 10:13:01 -0500

    delete RestTemplate

--- src/main/java/edu/escuelaing/arsw/tictactoe/TicTacToeApplication.java
+++ src/main/java/edu/escuelaing/arsw/tictactoe/TicTacToeApplication.java
@@ -3,26 +3,14 @@ package edu.escuelaing.arsw.tictactoe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class TicTacToeApplication {
 
-
-
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        RestTemplate restTemplate = builder.build();
-        return restTemplate;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeApplication.class, args);
