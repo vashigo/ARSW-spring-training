103ec258e730baeae3d005ee1cac88713a72dfc2
Andres Vasquez <andres.vasquez@hydra.cloud>
2020-06-18 10:49:23 -0500

    delete jdbc

--- src/main/java/edu/escuelaing/arsw/tictactoe/TicTacToeApplication.java
+++ src/main/java/edu/escuelaing/arsw/tictactoe/TicTacToeApplication.java
@@ -1,16 +1,11 @@
 package edu.escuelaing.arsw.tictactoe;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 
 @SpringBootApplication
 public class TicTacToeApplication {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeApplication.class, args);
